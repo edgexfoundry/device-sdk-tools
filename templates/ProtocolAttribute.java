2254114e2e39f6d46fb866e7f093e85fdcd9d370
Tyler Cox <tyler_cox@dell.com>
2017-08-15 20:18:10 +0000

    Cleans up Attributes Class
    Removed old debug output
    
    Signed-off-by: Tyler Cox <tyler_cox@dell.com>

--- templates/ProtocolAttribute.java
+++ templates/ProtocolAttribute.java
@@ -36,13 +36,11 @@ public class ${Protocol name}Attribute {
   public ${Protocol name}Attribute(Object attributes) {
     try {
       Gson gson = new Gson();
-      // 1. Java object to JSON, and save into a file
       String jsonString = gson.toJson(attributes);
       ${Protocol name}Attribute thisObject = gson.fromJson(jsonString, this.getClass());
       ${CustomAttributesInitialization}
 
     } catch (Exception e) {
-      //e.printStackTrace();
       logger.error("Cannot Construct ${Protocol name}Attribute: " + e.getMessage());
     }
   }
